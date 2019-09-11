5b3c03ecd1f3419892767f298a99f51aa46865e6
wdesenvmoreira <wdesenvmoreira@gmail.com>
2019-09-11 19:52:46 -0300

    Inicio das alterações boas praticas trycatch

--- indicadores/dashbord.js
+++ indicadores/dashbord.js
@@ -8,11 +8,16 @@ async function postarInd() {
     // var dados = await allIndicadores('todos')
     var dados = await allIndicadores()
     var dadoIndicadores = dados.indicadores
-    dadoIndicadores.forEach(async function(campo, indice, arrayCompleta) {
-        var funcaoDados = verficarFuncaoIndicador(campo.modelo)
-        let funcaoBuscaDados = await verificarFuncaoBuscarDados(campo.buscarDados)
-        var dadosInd = funcaoBuscaDados.dadosIndLinha
-        var cabecalho = funcaoBuscaDados.cabecalho
-        postarIndicador(funcaoDados, cabecalho, dadosInd, campo.optionsInd)
-    })
+    try {
+        dadoIndicadores.forEach(async function(campo, indice, arrayCompleta) {
+            var funcaoDados = verficarFuncaoIndicador(campo.modelo)
+            let funcaoBuscaDados = await verificarFuncaoBuscarDados(campo.buscarDados)
+            var dadosInd = funcaoBuscaDados.dadosIndLinha
+            var cabecalho = funcaoBuscaDados.cabecalho
+            postarIndicador(funcaoDados, cabecalho, dadosInd, campo.optionsInd)
+        })
+    } catch (error) {
+        console.log('Erro:', error, 'Erro em: funcao postarInd - indicadores/dashbord.js')
+    }
+
 }
